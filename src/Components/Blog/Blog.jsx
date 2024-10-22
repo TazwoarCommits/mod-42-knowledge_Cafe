acc877901461e2232051d3013620ff4958e76337
TazwoarCommits <tazwoarlives@gmail.com>
2024-10-22 16:01:56 +0600

    set up each blogs cover and author

--- src/Components/Blog/Blog.jsx
+++ src/Components/Blog/Blog.jsx
@@ -7,8 +7,14 @@ const Blog = ({blog}) => {
     
     return (
         <div className=''>
-            <img src={cover} alt="" />
-           <h3>{title}</h3>
+            <img className='rounded-2xl' src={cover} alt="" />
+            <div className='flex justify-between'>
+                <img className='w-12 rounded-full' src={author_img} alt="" />
+                <div>
+                <p>{reading_time} min read time</p>
+                </div>
+            </div>
+           <h3 className='text-3xl font-semibold'>{title}</h3>
         </div>
     );
 };
